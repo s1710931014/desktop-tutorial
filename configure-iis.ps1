02bfef29151805c63495a9979a9d85a042e6fbeb
s1710931014 <s1710931014@ad1.nutc.edu.tw>
2020-11-13 21:31:23 +0800

    add

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><body><h2>Welcome to Azure! My name is paul 1710931014</h2></body></html>'
+  -Value '<html><body><h2>Welcome to Azure! My name is Wei Ji-Hung 1710931014</h2><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMjhjOWVhMzMtMWFjYS00NDY4LTg0ZGUtN2Y4NTU4ZTA2YmU2IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
