e2298016ad71c34a752613eff7138b9f13529262
s1710931014 <s1710931014@ad1.nutc.edu.tw>
2020-11-13 15:34:11 +0800

    test1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Welcome to Azure! My name is paul 1710931014$($env:computername).</h2></body></html>"
+  -Value '<html><body><h2>Welcome to Azure! My name is paul 1710931014</h2></body></html>'
